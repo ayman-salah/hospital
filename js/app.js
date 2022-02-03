a8025a940284345e113bdd0d06f17709d671838e
ayman-salah <aymanelnajar2095@gmail.com>
2022-02-04 00:47:04 +0200

    upload all files for the first time

---
+++ js/app.js
@@ -0,0 +1,80 @@
+$(document).ready(function(){
+
+    $('.nav-icon ').click(function(){
+       $(this).toggleClass('fa-times')
+        $('.container__mynav').toggleClass('nav-toggle')
+    })
+
+    $(window).on('load scroll' , function(){
+        $('.nav-icon').removeClass('fa-times');
+        $('.container__mynav').removeClass('nav-toggle');
+
+        if($(window).scrollTop() > 10){
+            $('.header').addClass('header-active');
+        }
+        else{
+            $('.header').removeClass('header-active');
+        }
+    })
+
+// init plugin to make popup 
+    $('.facility').magnificPopup({
+        delegate:'a',
+        type:'image',
+        gallery:{
+            enabled:true
+        }
+    });
+
+
+    // init plugin to make animation while scroll 
+    AOS.init({
+        duration:500,
+        delay:400
+    });
+    // code of counter up 
+
+   
+})
+
+// start code of pure javascript 
+// end jquery code 
+//  start pure javascript code 
+
+// this code for count up the number in section counter while scroll for the first time 
+const timer = document.querySelectorAll('.timer');
+const counter_div = document.querySelector('#counter');
+var state_of_counter_method = false;
+
+
+window.addEventListener('DOMContentLoaded',function(){
+    console.log(window.scrollY)
+ })
+
+window.onscroll = function(){
+    
+    if(window.scrollY >= counter_div.offsetTop - 200){
+        if(!state_of_counter_method){
+            count_up(timer);
+            state_of_counter_method = true;
+        }
+       
+    }
+
+}
+
+
+function count_up (el){
+  el.forEach(element => {
+    var goal = element.dataset.goal;
+    let count = setInterval(() => {
+        element.textContent++;
+        if(element.textContent == goal){
+            clearInterval(count);
+        }
+    },1.6);
+    console.log(2000/goal);
+  });
+  
+   
+}
